720b4482eb9af5f461b43685c746531059a6bf7a
user20112 <devlinpaddock@gmail.com>
2019-04-22 19:18:56 -0400

    Deployed newerst version
    Some Android Manifest changes for deploying the new version.

---
+++ IdleSwarm/IdleSwarm.Android/obj/Debug/81/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "66d1e54b-e33f-41fd-ab88-4f3642b9104c",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "Xamarin.Android.Net.AndroidClientHandler",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep",
+
+	};
+}
